e36af3ab729627ecdf9e663c8af8b050c18b3eaa
waniaakhtar <145915590+waniaakhtar@users.noreply.github.com>
2023-12-04 20:31:13 +0100

    Update inference.py

--- inference.py
+++ inference.py
@@ -6,17 +6,16 @@ from torchvision import transforms
 from torch.autograd import Variable
 import numpy as np
 
-
 image_path = ''
 
-# Load the pre-trained model (you'll need to replace 'trained_model.pt' with the actual path to your model file)
+# Das vortrainierte Modell laden (Man muss "trained_model.pt" durch den tatsächlichen Pfad zu der Modelldatei ersetzen)
 model = torch.load('trained_model.pt', map_location="cpu")
 model.eval()
 
-# Define class names for the possible image classifications to be done
+# Klassenbezeichnungen für mögliche Bildklassifikationen definieren
 class_names = ["infected", "normal"]
 
-# Define a series of image transformations to be applied to each loaded image (same as done for training images)
+# Reihe von Bildtransformationen definieren, die auf jedes geladene Bild angewendet werden sollen (genauso wie bei den Trainingsbildern durchgeführt)
 loader = transforms.Compose([
     transforms.Resize(224),
     transforms.ToTensor(),
@@ -24,54 +23,50 @@ loader = transforms.Compose([
     transforms.Normalize([0.252, 0.293, 0.288], [0.146, 0.191, 0.193])
 ])
 
-# Define a function to load an image, preprocess it, and convert it to a tensor
+#  Funktion definieren, um ein Bild zu laden, es vorzubereiten und in einen Tensor umzuwandeln
 def image_loader(image):
     """Load an image and return it as a CUDA tensor (assumes GPU usage)"""
-    try:
-        image = Image.open(image)  # Open the image file
-        image = loader(image).float()  # Apply the defined transformations to the image
-        image = Variable(image, requires_grad=True)  # Create a PyTorch variable with gradients enabled
-        image = image.unsqueeze(0)  # Reshape the image tensor (not necessary for ResNet)
-        return image  # Return the preprocessed image tensor
-    except:
-        st.markdown(f"<p style='color:red;font-size:20px;text-align:center;'>Fehler: Das ist kein Röntgenbild von einer Lunge</p>", unsafe_allow_html=True)
-        exit()
+    image = Image.open(image)  # Bilddatei öffnen
+    image = loader(image).float()  # definierte Transformationen auf das Bild auftragen
+    image = Variable(image, requires_grad=True)  # PyTorch-Variable mit aktivierten Gradienten erstellen
+    image = image.unsqueeze(0)  # Bildtensor umformen (für ResNet nicht notwendig)
+    return image  # Die vorverarbeitete Bildmatrix zurückgeben
 
-# Create a Streamlit web app
+# Streamlit web app erstellen
 st.title("COVID-19 Classifier")
 
-# Upload an image
+# Bild hochladen
 uploaded_image = st.file_uploader("Upload X-Ray Image", type=["jpg", "png", "jpeg"])
 
 if uploaded_image:
-    # Display the uploaded image
+    # Hochgeladenes Bild anzeigen
     st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
 
-    # Perform predictions when a button is clicked
+    # Predictions ausführen, wenn auf eine Schaltfläche geklickt wird
     if st.button("Classify"):
         image_path = "temp.jpg"  # Temporary image path
 
-        # Save the uploaded image to a temporary file
+        # Das hochgeladene Bild in einer temporären Datei speichern
         with open(image_path, "wb") as f:
             f.write(uploaded_image.read())
 
-        # Load and preprocess the uploaded image
+        #Das hochgeladene Bild laden und vorverarbeiten
         image = image_loader(image_path)
 
-        # Perform a prediction using the loaded model
+        # Eine Vorhersage unter Verwendung des geladenen Modells durchführen
         output = model(image)
         index = output.data.cpu().numpy().argmax()
         predicted_class = class_names[index]
 
-        # Determine the color for the classification label (red for infected, green for normal)
+        #Die Farbe für das Klassifikationslabel bestimmen (Rot für infiziert, Grün für normal)
         color = "red" if predicted_class == "infected" else "green"
 
-        # Display the result in text form with the determined color
+        #  Ergebnis in Textform mit der festgelegten Farbe anzeigen
         st.markdown(
             f"<p style='color:{color};font-size:20px;text-align:center;'>Classified as {predicted_class}</p>",
             unsafe_allow_html=True,
         )
 
-# Clean up the temporary image file
+# Temporäre Bilddatei aufräumen
 if os.path.exists(image_path):
     os.remove(image_path)
